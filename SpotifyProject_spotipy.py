ed7625808f466da641fe80a53cc30736d1a6f7a7
Felix Lu <itzfelixlu98@gmail.com>
2024-11-25 22:55:08 -0800

    Add search block to original file
    Will test functionality after rendering html templates

--- SpotifyProject_spotipy.py
+++ SpotifyProject_spotipy.py
@@ -41,6 +41,79 @@ def callback():
     sp_oauth.get_access_token(request.args['code'])
     return redirect(url_for('get_playlists'))
 
+#Will test this block of code when rendered html templates
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if not sp_oauth.validate_token(cache_handler.get_cached_token()):
+        auth_url = sp_oauth.get_authorize_url()
+        return redirect(auth_url)
+    
+    results = None
+    if request.method == 'POST':
+        query = request.form.get('query', '')
+        search_type = request.form.get('type', 'artist')
+    
+        if query:
+            spotify_results = sp.search(q=query, type=search_type, limit=10)
+        
+            # Handle artist search
+            if search_type == 'artist':
+                if spotify_results['artists']['items']:
+                    artist = spotify_results['artists']['items']
+                    artist_id = artist['id']
+                    artist_name = artist['name']
+                    artist_url = artist['external_urls']['spotify']
+                    artist_image = artist['images'][0]['url'] if artist['images'] else None
+                    top_tracks = sp.artist_top_tracks(artist_id)['tracks']
+                    results = {
+                        'name': artist_name,
+                        'url': artist_url,
+                        'image': artist_image,
+                        'top_tracks': [
+                            {'name': track['name'], 'url': track['external_urls']['spotify']}
+                            for track in top_tracks
+                        ]
+                    }
+                else:
+                    results = {'type': 'error', 'message': 'No artist found.'}
+
+            elif search_type == 'track':
+                    tracks = spotify_results['tracks']['items']
+                    if tracks:  # Check if track results exist
+                        results = {
+                            'type': 'track',
+                            'tracks': [
+                                {
+                                    'name': f"{track['name']} by {', '.join(artist['name'] for artist in track['artists'])}",
+                                    'url': track['external_urls']['spotify']
+                                }
+                                for track in tracks
+                            ]
+                        }
+                    else:
+                        results = {'type': 'error', 'message': 'No tracks found.'}
+
+            elif search_type == 'album':
+                albums = spotify_results['albums']['items']
+                if albums:  # Check if album results exist
+                    results = {
+                        'type': 'album',
+                        'albums': [
+                            {
+                                'name': album['name'],
+                                'url': album['external_urls']['spotify'],
+                                'release_date': album['release_date']
+                            }
+                            for album in albums
+                        ]
+                    }
+                else:
+                    results = {'type': 'error', 'message': 'No albums found.'}
+    
+    # Render the search results page with the results
+    #return render_template('search.html', results=results)
+
+
 @app.route('/get_playlists')
 def get_playlists():
     if not sp_oauth.validate_token(cache_handler.get_cached_token()):
